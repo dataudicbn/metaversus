94c6872af731c415b1986c1f4e93cc927e87208e
Nguyen Van Dat <dataudicbn@gmai.com>
2023-01-07 16:29:52 +0700

    metaversus

---
+++ app/page.js
@@ -0,0 +1,32 @@
+import { Footer, Navbar } from "../components";
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from "../sections";
+
+const Page = () => (
+  <div className="overflow-hidden bg-primary-black">
+    <Navbar />
+    <Hero />
+    <div className="relative">
+      <About />
+      <div className="z-0 gradient-03" />
+      <Explore />
+    </div>
+    <Explore />
+    <GetStarted />
+    <WhatsNew />
+    <World />
+    <Insights />
+    <Feedback />
+    <Footer />
+  </div>
+);
+
+export default Page;
---
+++ components/Navbar.jsx
@@ -0,0 +1,23 @@
+"use client";
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative `}
+  >
+    <div className="absolute inset-0 w-1/2 " />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img src="/search.svg" alt="search" className="object-contain w-6 h-6" />
+      <h2 className="font-extrabold text-[24px] leading-8 text-white">
+        METAVERSUSs
+      </h2>
+      <img src="/menu.svg" alt="menu" className="object-contain w-6 h-6" />
+    </div>
+  </motion.nav>
+);
+
+export default Navbar;

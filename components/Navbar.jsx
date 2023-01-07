74bd3177493559e58d3d5cfaf4ac6127b981219d
Nguyen Van Dat <dataudicbn@gmai.com>
2023-01-07 16:58:18 +0700

    prettierrc

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,6 +3,7 @@
 import { motion } from 'framer-motion';
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
   <motion.nav
     variants={navVariants}
